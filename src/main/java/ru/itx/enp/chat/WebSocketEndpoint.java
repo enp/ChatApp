bbeb623bf3372ba81de744dd98c272c719dd0f95
Eugene Prokopiev <enp@itx.ru>
2020-04-23 09:25:03 +0300

    explicit connect/send/disconnect in client + user names support

--- src/main/java/ru/itx/enp/chat/WebSocketEndpoint.java
+++ src/main/java/ru/itx/enp/chat/WebSocketEndpoint.java
@@ -2,44 +2,45 @@ package ru.itx.enp.chat;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@ServerEndpoint("/socket")
+@ServerEndpoint("/socket/{user}")
 public class WebSocketEndpoint {
 	private Logger logger = LoggerFactory.getLogger(WebSocketEndpoint.class);
-	private static Set<Session> connections = Collections.synchronizedSet(new HashSet<>());
+	private static Map<String,Session> connections = Collections.synchronizedMap(new HashMap<>());
 
 	@OnOpen
-	public void onOpen(Session session) {
-		logger.info("Open session: {}", session.getId());
-		connections.add(session);
+	public void onOpen(Session session, @PathParam("user") String user) {
+		logger.info("Open session: {} from user {}", session.getId(), user);
+		connections.put(user, session);
 	}
 
 	@OnMessage
-	public void onMessage(Session session, String message) throws IOException {
-		logger.info("Receive message: {} in session {}", message, session.getId());
-		for (Session connection : connections) {
-			logger.info("Found session {} in connections", connection.getId());
-			if (!connection.equals(session)) {
-				logger.info("Send message {} to session {}", message, connection.getId());
-				connection.getBasicRemote().sendText(message);
+	public void onMessage(Session session, @PathParam("user") String from, String message) throws IOException {
+		logger.info("Receive message: {} in session {} from user {}", message, session.getId(), from);
+		for (String user : connections.keySet()) {
+			logger.info("Found session {} in connections", connections.get(user).getId());
+			if (!connections.get(user).equals(session)) {
+				logger.info("Send message {} to session {}", message, connections.get(user).getId());
+				connections.get(user).getBasicRemote().sendText(String.format("%s: %s", from, message));
 			}
 		}
 	}
 
 	@OnClose
-	public void onClose(Session session) {
-		logger.info("Close session: {}", session.getId());
-		connections.remove(session);
+	public void onClose(Session session, @PathParam("user") String user) {
+		logger.info("Close session: {} from user {}", session.getId(), user);
+		connections.remove(user);
 	}
 }
