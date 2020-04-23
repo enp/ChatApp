87099aa07e92cf810a8b76548dd47fa045ab325c
Eugene Prokopiev <enp@itx.ru>
2020-04-23 08:43:48 +0300

    send messages between connections

--- src/main/java/ru/itx/enp/chat/WebSocketEndpoint.java
+++ src/main/java/ru/itx/enp/chat/WebSocketEndpoint.java
@@ -1,13 +1,45 @@
 package ru.itx.enp.chat;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ServerEndpoint("/socket")
 public class WebSocketEndpoint {
-    @OnMessage
-    public String handleTextMessage(String message) {
-        System.out.println("New Text Message Received");
-        return message;
-    }
+	private Logger logger = LoggerFactory.getLogger(WebSocketEndpoint.class);
+	private static Set<Session> connections = Collections.synchronizedSet(new HashSet<>());
+
+	@OnOpen
+	public void onOpen(Session session) {
+		logger.info("Open session: {}", session.getId());
+		connections.add(session);
+	}
+
+	@OnMessage
+	public void onMessage(Session session, String message) throws IOException {
+		logger.info("Receive message: {} in session {}", message, session.getId());
+		for (Session connection : connections) {
+			logger.info("Found session {} in connections", connection.getId());
+			if (!connection.equals(session)) {
+				logger.info("Send message {} to session {}", message, connection.getId());
+				connection.getBasicRemote().sendText(message);
+			}
+		}
+	}
+
+	@OnClose
+	public void onClose(Session session) {
+		logger.info("Close session: {}", session.getId());
+		connections.remove(session);
+	}
 }
